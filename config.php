aea5d819658576246d77d2243524520ca334abd4
nthans <nathanlammers01@gmail.com>
2025-01-13 11:42:30 +0100

    web

--- config.php
+++ config.php
@@ -1,21 +1,15 @@
 <?php
 session_start();
 
-/**
- * Voor de MAC gebruikers;
- */
+
+
+
+//Voor de Windows gebruikers;
+
 $dbhost = "localhost";
 $dbuser = "root";
-$dbpass = "root";
-$dbname = "youtube-test";
-
-/**
- * Voor de Windows gebruikers;
- */
-// $dbhost = "localhost";
-// $dbuser = "root";
-// $dbpass = "";
-// $dbname = "youtube-clone";
+$dbpass = "";
+$dbname = "youtube-clone";
 
 $con = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
